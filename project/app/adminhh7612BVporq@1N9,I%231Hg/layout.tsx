319d881644d95ea1e5b974554f8e37f8c91f5a54
Hasan Abbas <146252861+hasanjami@users.noreply.github.com>
2026-06-11 14:21:50 +0300

    Delete project/app directory

--- project/app/adminhh7612BVporq@1N9,I%231Hg/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Admin Dashboard — 7snjami',
-}
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
